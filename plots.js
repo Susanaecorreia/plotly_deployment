c88f9fd75929823b79cfb1e55c06a659e935c798
Susanaecorreia <78492568+Susanaecorreia@users.noreply.github.com>
2021-05-11 19:30:55 -0400

    Challenge12

--- plots.js
+++ plots.js
@@ -1,22 +1,97 @@
-var sortedCities = cityGrowths.sort((a, b) =>
-  a.Increase_from_2016 - b.Increase_from_2016).reverse();
+// var sortedCities = cityGrowths.sort((a, b) =>
+//   a.Increase_from_2016 - b.Increase_from_2016).reverse();
 
-var topFiveCities = sortedCities.slice(0, 5);
+// var topFiveCities = sortedCities.slice(0, 5);
 
-var topFiveCityNames = topFiveCities.map(city => city.City);
-var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
+// var topFiveCityNames = topFiveCities.map(city => city.City);
+// var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
 
-var trace = {
-  x: topFiveCityNames,
-  y: topFiveCityGrowths,
-  type: "bar"
-};
-var data = [trace];
-var layout = {
-  title: "Most Rapidly Growing Cities",
-  xaxis: { title: "City" },
-  yaxis: { title: "Population Growth, 2016-2017" }
-};
-Plotly.newPlot("bar-plot", data, layout);
+// var trace = {
+//   x: topFiveCityNames,
+//   y: topFiveCityGrowths,
+//   type: "bar"
+// };
+// var data = [trace];
+// var layout = {
+//   title: "Most Rapidly Growing Cities",
+//   xaxis: { title: "City" },
+//   yaxis: { title: "Population Growth, 2016-2017" }
+// };
+// Plotly.newPlot("bar-plot", data, layout);
 
 // console.log(cityGrowths);
+
+//------------------------------------------------
+//dataset1 and dataset2 shows 2 graphs
+//------------------------------------------------
+
+// function init() {
+//   data = [{
+//     x: [1, 2, 3, 4, 5],
+//     y: [1, 2, 4, 8, 16]
+//   }];
+//   Plotly.newPlot("plot", data);
+// };
+
+// d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+// function updatePlotly() {
+//   var dropdownMenu = d3.select("#dropdownMenu");
+//   var dataset = dropdownMenu.property("value");
+
+//   var xData = [1, 2, 3, 4, 5];
+//   var yData = [];
+
+//   if (dataset === 'dataset1') {
+//     yData = [1, 2, 4, 8, 16];
+//   };
+
+//   if (dataset === 'dataset2') {
+//     yData = [1, 10, 100, 1000, 10000];
+//   };
+
+//   var trace = {
+//     x: [xData],
+//     y: [yData],
+//   };
+//   Plotly.restyle("plot", trace);
+// };
+
+// init();
+
+//---------------------------------------------------
+// bellybutton menu
+//---------------------------------------------------
+
+function init() {
+  var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    function buildMetadata(sample) {
+      d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+
+        PANEL.html("");
+        PANEL.append("h6").text(result.location);
+      });
+    }
+    function optionChanged(newSample) {
+      buildMetadata(newSample);
+      buildCharts(newSample);
+    }
+
+  })
+}
+
+init();
